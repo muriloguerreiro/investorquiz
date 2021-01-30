3cc0ac110dd86931e46c3dd662598fee2517cdd1
Murilo Guerreiro Badoco <guerreiro_murilo@hotmail.com>
2021-01-30 01:45:03 -0300

    End of class 4

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
@@ -9,17 +8,9 @@ import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
   const router = useRouter();
@@ -27,35 +18,36 @@ export default function Home() {
 
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>{db.title}</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://investorquiz.muriloguerreiro.vercel.app/" />
+        <meta property="og:title" content="Investor Quiz" />
+        <meta property="og:description" content="Test your knowledge and find out if you are a good investor!" />
+        <meta property="og:image" content="https://cdn.pixabay.com/photo/2017/09/07/08/54/money-2724241_1280.jpg" />
+      </Head>
       <QuizContainer>
-        <Head>
-          <title>Investor Quiz</title>
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://investorquiz.muriloguerreiro.vercel.app/" />
-          <meta property="og:title" content="Investor Quiz" />
-          <meta property="og:description" content="Test your knowledge and find out if you are a good investor!" />
-          <meta property="og:image" content="https://cdn.pixabay.com/photo/2017/09/07/08/54/money-2724241_1280.jpg" />
-        </Head>
         <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function (e) {
-              e.preventDefault();
+            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
               router.push(`/quiz?name=${name}`);
             }}
             >
-              <input
-                onChange={function (e) {
-                  setName(e.target.value);
-                }}
+              <Input
+                name="userName"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
                 placeholder="Enter your name..."
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Play
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Play as ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
